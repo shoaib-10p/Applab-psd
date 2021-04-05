8906ce831d1f1b2c8654c0215659a320963bfda3
shoaib-10p <79453991+shoaib-10p@users.noreply.github.com>
2021-04-05 09:59:54 +0500

    Add files via upload

---
+++ dist/index.dev.js
@@ -0,0 +1,12 @@
+// "use strict";
+
+// function hambbtn() {
+//   var hmbtn = document.querySelector("ul");
+
+//   if (hmbtn.style.top === "-330px") {
+//     hmbtn.style.display = "block";
+//     hmbtn.style.top = "56px";
+//   } else {
+//     hmbtn.style.top = "-330px";
+//   }
+// }
