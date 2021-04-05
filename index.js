f235dc54a81333cdd5115c15b98cd134d61dcbf1
shoaib-10p <79453991+shoaib-10p@users.noreply.github.com>
2021-04-05 10:33:30 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+function hambbtn(){
+let hmbtn = document.querySelector("ul");
+if ( hmbtn.style.top === "-330px") {
+    hmbtn.style.display = "block";
+    hmbtn.style.top= "56px";
+   
+}
+else{
+
+    hmbtn.style.top = "-330px";
+}
+}
